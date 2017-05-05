ce31d209c5f5e9ea9b6c5040f33d6f844337877c
Xiang Liu <liuxiang@umich.edu>
2017-05-05 11:24:00 -0400

    split raw added

--- splitTrainTest.m
+++ splitTrainTest.m
@@ -1,14 +1,18 @@
-function [ train,test ] = splitTrainTest( data, ratio )
+function [ train,test,trainRaw,testRaw ] = splitTrainTest( data,dataRaw ratio )
 %SPLITTRAINTEST Summary of this function goes here
 %   Detailed explanation goes here
 train=data(1,:);
+trainRaw=dataRaw(1,:);
 test=data(1,:);
+testRaw=dataRaw(1,:);
 rng('shuffle')
 for i=2:size(data,1)
 if rand()<=ratio
     train=[train;data(i,:)];
+	trainRaw=[trainRaw;dataRaw(i,:)];
 else
      test=[test;data(i,:)];
+ 	testRaw=[testRaw;dataRaw(i,:)];
 end
 end
 
