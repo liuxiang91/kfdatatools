29cbd972a005e65be6e3ab67f0536a7c7e6f660d
Xiang Liu <liuxiang.icit@gmail.com>
2017-12-07 14:42:44 -0500

    Update frontBackTrim.m

--- frontBackTrim.m
+++ frontBackTrim.m
@@ -21,10 +21,10 @@ for i=2:size(data,1)
     while data{i,2}(end)- data{i,2}(end-1)>24
         for j=2:size(data,2)
             if j==3
-                data{i,3}=data{i,3}(:,2:end);
+                data{i,3}=data{i,3}(:,1:end-1);
                 continue;
             end
-            data{i,j}=data{i,j}(2:end);
+            data{i,j}=data{i,j}(1:end-1);
         end
         if  length(data{i,2})==1
             break;
